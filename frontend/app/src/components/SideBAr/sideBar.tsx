cfd728d2db74f6ee3d9ff6af162c165ba22c7a06
divyaksh . <divyaksh1552002@gmail.com>
2024-08-23 00:11:29 +0530

    sliding-feature

--- frontend/app/src/components/SideBAr/sideBar.tsx
+++ frontend/app/src/components/SideBAr/sideBar.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import styles from "./sideBAr.module.css";
 
 interface SideBarProps {
@@ -5,15 +6,38 @@ interface SideBarProps {
 }
 
 const SideBar: React.FC<SideBarProps> = ({ handleNewChat }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleSideBar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <div className={styles.sideBarContainer}>
+      <div className={`${styles.sideBarContainer} ${isOpen ? styles.open : styles.closed}`}>
+        <span
+          className="material-symbols-outlined"
+          onClick={toggleSideBar}
+          style={{ cursor: "pointer", color: "white" }}
+        >
+          arrow_circle_left
+        </span>
         <div>
           <button onClick={handleNewChat} className={styles.newChatButton}>
             New Chat
           </button>
         </div>
       </div>
+      {!isOpen && (
+        <button onClick={toggleSideBar} className={styles.openSideBarButton}>
+          <span
+            className="material-symbols-outlined"
+            style={{ cursor: "pointer", color: "white" }}
+          >
+            arrow_circle_right
+          </span>
+        </button>
+      )}
     </>
   );
 };
